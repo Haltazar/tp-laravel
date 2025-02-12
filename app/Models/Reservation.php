6b645761497ee022c6fc462bce187634e117e2d3
Haltazar <166007444+Haltazar@users.noreply.github.com>
2025-02-12 16:30:12 +0100

    [Feat] - Création du model de résa

---
+++ app/Models/Reservation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'owner_id',
+        'box_id',
+        'start_at',
+        'end_at',
+        'price',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function box()
+    {
+        return $this->belongsTo(Box::class);
+    }
+}
